c82ac58e9b819fe60f265782b3d2658c4fa2d0b3
Edmundo0994 <esala.094@gmail.com>
2020-07-06 18:22:16 -0500

    Uso de http para crear un servidor en un puerto

---
+++ modulos/http.js
@@ -0,0 +1,28 @@
+const http = require('http')
+
+http.createServer(router).listen(3000)
+
+function router(request, response) {
+  console.log('Nueva petición!')
+  console.log(request.url)
+
+  switch (request.url){
+    case '/hola':
+      response.write('Hola, que tal?')
+      response.end()
+      break
+    default:
+      response.write('Error 404, no se lo que quieres')
+      response.end()
+  }
+
+  // response.writeHead(201, {'Content-type': 'text/plain'})
+
+  // // Escribir respuesta al usuario
+  // response.write('Hola, ya se usar HTTP de NodeJs')
+
+  // response.end()
+}
+
+
+console.log("Esuchando http en el puerto 3000")
